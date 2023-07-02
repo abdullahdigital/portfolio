1646143c05fd971e69ae00043b615462f4ab4f95
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2023-07-02 18:04:08 +0500

    portfolio

---
+++ style.js
@@ -0,0 +1,34 @@
+// Scroll Reveal
+ScrollReveal({ 
+    reset: true ,
+    distance: '80px' ,
+    duration: 2000 ,
+    delay: 200 
+});
+ScrollReveal().reveal('.home-content , .h2', { origin: 'top' });
+ScrollReveal().reveal('.home-contentdown', { origin: 'bottom' });
+ScrollReveal().reveal('.left', { origin: 'left' });
+ScrollReveal().reveal('.right', { origin: 'right' });
+
+
+// Type Js
+const typed = new Typed('.multiple-text', {
+    strings: ['FrontEnd Developer', 'UI/UX Designer', 'Creative Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+
+ 
+  
+
+
+  
+  
+  
+  
+  
+  
+  
+  
