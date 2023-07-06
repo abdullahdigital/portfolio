d5be0e868c57e330195820eea8f8c27855ff640b
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2023-07-06 12:00:19 +0500

    added links

--- style.js
+++ style.js
@@ -20,6 +20,43 @@ const typed = new Typed('.multiple-text', {
     loop: true
   });
 
+
+
+  //dark and light
+  let btn=document.getElementById("btn5")
+  let btnText=document.getElementById("btnText")
+  let btnIcon=document.getElementById("btnIcon")
+
+
+  btn5.onclick=function(){
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        btnIcon.src="img/sun1.png"
+        btnText.innerHTML="Light";
+    }else{
+        btnIcon.src="img/moon2.png"
+        btnText.innerHTML="Dark";
+    }
+  }
+
+  
+
+  
+  
+  
+
+
+
+
+ 
+  
+  
+
+  
+  
+  
+
  
   
 
